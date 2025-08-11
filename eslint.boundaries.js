fedef14869553a4218b47af0f7f9fc5f7f5924e9
pvslvch <polina.vasilevichh@gmail.com>
2025-08-11 15:41:28 +0200

    setting up eslint

---
+++ eslint.boundaries.js
@@ -0,0 +1,71 @@
+import boundaries from "eslint-plugin-boundaries";
+
+export const eslintBoundariesConfig = {
+  plugins: {
+    boundaries,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+
+    "boundaries/elements": [
+      {
+        type: "app",
+        pattern: "./src/app",
+      },
+      {
+        type: "features",
+        pattern: "./src/features/*",
+      },
+
+      {
+        type: "shared",
+        pattern: "./src/shared",
+      },
+    ],
+  },
+  rules: {
+    "boundaries/element-types": [
+      2,
+      {
+        default: "allow",
+        rules: [
+          {
+            from: "shared",
+            disallow: ["app", "features"],
+            message:
+              "The underlying layer module (${file.type}) cannot import upstream module (${dependency.type})",
+          },
+          {
+            from: "features",
+            disallow: ["app"],
+            message:
+              "Downstream module (${file.type}) cannot import upstream module (${dependency.type})",
+          },
+        ],
+      },
+    ],
+    "boundaries/entry-point": [
+      2,
+      {
+        default: "disallow",
+        message:
+          "Module (${file.type}) must be imported via public API. Direct import from ${dependency.source} is not allowed",
+
+        rules: [
+          {
+            target: ["shared", "app"],
+            allow: "**",
+          },
+          {
+            target: ["features"],
+            allow: "index.(ts|tsx)",
+          },
+        ],
+      },
+    ],
+  },
+};
