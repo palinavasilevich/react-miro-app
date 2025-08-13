04d7c0b1a329f9de0e2adf043740aa77faff2c40
pvslvch <polina.vasilevichh@gmail.com>
2025-08-13 13:58:30 +0200

    feat[auth]: implement login page UI

---
+++ src/features/auth/auth-layout.tsx
@@ -0,0 +1,40 @@
+import type { ReactNode } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/shared/ui/kit/card";
+
+type Props = {
+  children: ReactNode;
+  title: ReactNode;
+  description: ReactNode;
+  footerText: ReactNode;
+};
+
+export function AuthLayout({
+  children,
+  title,
+  description,
+  footerText,
+}: Props) {
+  return (
+    <main className="grow flex flex-col pt-[200px] items-center">
+      <Card className="w-full max-w-[400px]">
+        <CardHeader>
+          <CardTitle className="text-center">{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent>{children}</CardContent>
+        <CardFooter>
+          <p className="text-sm text-muted-foreground [&_a]:underline [&_a]:text-primary">
+            {footerText}
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,5 +1,21 @@
+import { ROUTES } from "@/shared/model/routes";
+import { Link } from "react-router-dom";
+import { AuthLayout } from "./auth-layout";
+
 function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <AuthLayout
+      title="Login"
+      description="Enter your email and password"
+      footerText={
+        <>
+          Don't have an account yet? <Link to={ROUTES.REGISTER}>Sign up</Link>
+        </>
+      }
+    >
+      <form></form>
+    </AuthLayout>
+  );
 }
 
 export const Component = LoginPage;
