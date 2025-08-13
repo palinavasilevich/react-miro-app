990e9a36234552b4183b3a0f8103555aaedc2cb8
pvslvch <polina.vasilevichh@gmail.com>
2025-08-13 15:22:06 +0200

    feat[auth]: implement register form

---
+++ src/features/auth/register-form.tsx
@@ -0,0 +1,83 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/ui/kit/form";
+import { Input } from "@/shared/ui/kit/input";
+import { Button } from "@/shared/ui/kit/button";
+
+const registerSchema = z
+  .object({
+    email: z.string("Email is required").email("Invalid email"),
+    password: z
+      .string("Password is required")
+      .min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z.string().optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "The passwords do not match",
+  });
+
+export function RegisterForm() {
+  const form = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = form.handleSubmit((data) => console.log(data));
+
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="admin@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </Form>
+  );
+}
--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
 import { AuthLayout } from "./auth-layout";
 import { ROUTES } from "@/shared/model/routes";
+import { RegisterForm } from "./register-form";
 
 function RegisterPage() {
   return (
     <AuthLayout
       title="Sign Up"
       description="Enter your email and password password to register"
+      form={<RegisterForm />}
       footerText={
         <>
           Do you already have an account? <Link to={ROUTES.LOGIN}>Sign in</Link>
         </>
       }
-    >
-      <form></form>
-    </AuthLayout>
+    />
   );
 }
 
