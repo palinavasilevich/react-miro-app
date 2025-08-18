f4e16470769bf82a28116238902b05979e4f4228
pvslvch <polina.vasilevichh@gmail.com>
2025-08-18 15:03:09 +0200

    feat[router]: implement protected routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,9 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { AppHeader } from "@/features/header";
-import { ROUTES } from "@/shared/model/routes";
-import { Providers } from "./providers";
+import { Outlet } from "react-router-dom";
 
 export function App() {
-  const location = useLocation();
-
-  const isAuthPage =
-    location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
-
   return (
-    <Providers>
-      <div className="min-h-screen flex flex-col">
-        {!isAuthPage && <AppHeader />}
-        <Outlet />
-      </div>
-    </Providers>
+    <div className="min-h-screen flex flex-col">
+      <Outlet />
+    </div>
   );
 }
---
+++ src/app/router.tsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+import { App } from "./app";
+import { ROUTES } from "../shared/model/routes";
+import { ProtectedRoute } from "./protected-route";
+import { AppHeader } from "@/features/header";
+import { Providers } from "./providers";
+
+export const router = createBrowserRouter([
+  {
+    element: (
+      <Providers>
+        <App />
+      </Providers>
+    ),
+    children: [
+      {
+        element: (
+          <>
+            <AppHeader />
+            <ProtectedRoute />
+          </>
+        ),
+        children: [
+          {
+            path: ROUTES.BOARDS,
+            lazy: () => import("@/features/boards-list/boards-list.page"),
+          },
+          {
+            path: ROUTES.BOARD,
+            lazy: () => import("@/features/board/board.page"),
+          },
+        ],
+      },
+
+      {
+        path: ROUTES.LOGIN,
+        lazy: () => import("@/features/auth/login.page"),
+      },
+      {
+        path: ROUTES.REGISTER,
+        lazy: () => import("@/features/auth/register.page"),
+      },
+      {
+        path: ROUTES.HOME,
+        loader: () => redirect(ROUTES.BOARDS),
+      },
+    ],
+  },
+]);
